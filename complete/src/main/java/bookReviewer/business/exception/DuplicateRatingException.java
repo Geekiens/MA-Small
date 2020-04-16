dc0c4c7762c60ec66d63b5ae7c5bb7697ddeb287
Max Brämer <max.braemer@tu-dortmund.de>
2020-04-16 02:34:37 +0200

    custom exception

--- complete/src/main/java/bookReviewer/business/exception/DuplicateRatingException.java
+++ complete/src/main/java/bookReviewer/business/exception/DuplicateRatingException.java
@@ -1,4 +1,11 @@
 package bookReviewer.business.exception;
 
-public class DuplicateRatingException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class DuplicateRatingException extends RuntimeException{
+    public DuplicateRatingException() {
+        super("Rating of this user already exists for this book");
+    }
 }
