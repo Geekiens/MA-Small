5d9481f29c45f6389760c44ef3e91774d04f13d3
Max Brämer <max.braemer@tu-dortmund.de>
2020-06-01 16:03:26 +0200

    add Exceptions

--- complete/src/main/java/bookReviewer/business/exception/ForbiddenResourceException.java
+++ complete/src/main/java/bookReviewer/business/exception/ForbiddenResourceException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.FORBIDDEN)
-public class ForbiddenInteractionException extends RuntimeException{
-        public ForbiddenInteractionException() {
-            super("Role is not sufficient for this action");
+public class ForbiddenResourceException extends RuntimeException{
+        public ForbiddenResourceException() {
+            super("Resource can't be edited by thus user.");
         }
 }
