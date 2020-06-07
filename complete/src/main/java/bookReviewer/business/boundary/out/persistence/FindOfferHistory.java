c1674a953991d728e48dd1b7315220719c352718
Max Brämer <max.braemer@tu-dortmund.de>
2020-06-08 01:02:16 +0200

    correction of correction

--- complete/src/main/java/bookReviewer/business/boundary/out/persistence/FindOfferHistory.java
+++ complete/src/main/java/bookReviewer/business/boundary/out/persistence/FindOfferHistory.java
@@ -1,6 +1,6 @@
 package bookReviewer.business.boundary.out.persistence;
 
-import bookReviewer.business.shared.MediaType;
+import bookReviewer.persistence.model.MediaType;
 
 public interface FindOfferHistory {
     bookReviewer.persistence.model.CachedOfferHistoryPersistence findOfferHistory(String isbn, String vendor, MediaType mediaType);
